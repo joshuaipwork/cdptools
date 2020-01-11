607d30f3067bb9bfb2829889a286539e78de87c6
Joshua Ip <joshuaip@uw.edu>
2020-01-11 14:43:16 -0800

    Now imports times for each new speaker and captures a thumbnail for each new speaker

--- cdptools/thumbnail_generator/find_first_frame.py
+++ cdptools/thumbnail_generator/find_first_frame.py
@@ -1,6 +1,7 @@
 # import the necessary packages
 import face_recognition
 import cv2
+import math
 from typing import Tuple
 import argparse
 import pickle
@@ -51,62 +52,45 @@ transcript_save_path, video_save_path = get_video_and_transcript_for_event_id(ev
 print(transcript_save_path)
 print(video_save_path)
 stream = cv2.VideoCapture(video_save_path)
+writer = None
+
+# Get the times to collect thumbnails from
+speakerTimes = json.load(open(transcript_save_path, 'r'))
+
+# loop over frames from the video file stream
+for speaker in speakerTimes['data']:
+    # stream.read returns false if it was unable to get the next frame
+    # frame is a pointer to the frame,
+    (grabbed, frame) = (None, None)
+
+    # Get the middle time of the speaker's first sentence
+    timestamp = speaker['data'][0]['start_time'] + speaker['data'][0]['end_time'] / 2
+    print(timestamp)
+
+    # Convert the timestamp to the frame for cv2 to capture
+    fps = stream.get(cv2.CAP_PROP_FPS)
+    frame = math.floor(fps * timestamp)
+    stream.set(cv2.CAP_PROP_POS_FRAMES, frame)
+
+    # Get the frame
+    (grabbed, frame) = stream.read()
+
+    # if the frame was not grabbed, then something bad has happened
+    # I might throw an error here in the future
+    if not grabbed:
+        break
+
+    # detect the (x, y)-coordinates of the bounding boxes
+    # corresponding to each face in the input frame,
+    # then, if there is only one face we want to use that as the thumbnail
+    boxes = face_recognition.face_locations(frame)
+    print(len(boxes))
+
+    cv2.imwrite("./thumbnail" + str(timestamp) + ".png", frame)
 
 # close the video file pointers
 stream.release()
 
-writer = None
-pass
-
-# def frame_capture():
-#     cap = cv2.VideoCapture("./council_010620_2022001V.mp4")
-#     face_frame = False
-#     count = 60
-#     while not face_frame and count <150:
-#         cap.set(1, count)
-#         ret, frame = cap.read()
-#         cv2.imwrite("frame.jpg", frame)
-#         print(f'Frame {count} extracted')
-#         count += 60
-#     pass
-
-# loop over frames from the video file stream
-#
-
-
-#         while not cap.isOpened():
-#                 cap = cv2.VideoCapture("video.mp4")
-#                 cv2.waitKey(1000)
-#                 print "Wait for the header"
-
-# pos_frame = cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
-#     flag, frame = cap.read()
-#     if flag:
-#             # The frame is ready and already captured
-#             cv2.imshow('video', frame)
-#             pos_frame = cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
-#             print str(pos_frame)+" frames"
-#     else:
-#             # The next frame is not ready, so we try to read it again
-#             cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos_frame-1)
-#             print "frame is not ready"
-#             # It is better to wait for a while for the next frame to be ready
-#             cv2.waitKey(1000)
-
-#     if cv2.waitKey(10) == 27:
-#             break
-#     if cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES) == cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT):
-#             # If the number of captured frames is equal to the total number of frames,
-#             # we stop
-#             break
-
-#         cap = cv2.VideoCapture("video.mp4")
-# total_frames = cap.get(7)
-
-# Here 7 is the prop-Id. You can find more here http://docs.opencv.org/2.4/modules/highgui/doc/reading_and_writing_images_and_video.html
-
-# After that you can set the frame number, suppose i want to extract 100th frame
-
-# cap.set(1, 100)
-# ret, frame = cap.read()
-# cv2.imwrite("path_where_to_save_image", frame)
+# check to see if the video writer point needs to be released
+if writer is not None:
+    writer.release()
